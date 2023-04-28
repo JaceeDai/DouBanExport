5124da2c10bc31d139f2ad21c19f697923a61dae
Jacee <jacee_dai@163.com>
2023-04-28 15:33:00 +0800

    bridge broswer and local prgramming

---
+++ main_export.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         test-豆瓣导出 Dev
+// @namespace    test-userscript
+// @version      1.0.0
+// @include      https://www.douban.com/people/*
+// @run-at       document-end
+// @require      https://code.jquery.com/jquery-3.6.0.min.js
+// @require      file:///Users/jacee/projects/fromnet/_JS/DouBanExport/export.js
+// ==/UserScript==
+
+console.log("---main---");
